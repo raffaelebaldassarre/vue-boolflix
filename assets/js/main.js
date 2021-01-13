133683ea3d242c6e6c58847880d771a3b0d063bc
RaffaeleBaldassarre <raffaeleandrea.baldassarre@gmail.com>
2021-01-13 13:32:51 +0100

    add flags

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,24 @@ let app = new Vue ({
                 this.totalpages = response.data.total_pages;
                 movies.forEach(movie => {
                     let score = Math.ceil(movie.vote_average / 2);
-                    movie.vote_average = score;   
+                    movie.vote_average = score;
+                    if (movie.original_language == "en"){
+                        movie.original_language = "gb";
+                      } else if (movie.original_language == "zh") {
+                        movie.original_language = "cn"
+                      } else if (movie.original_language == "ko") {
+                        movie.original_language = "kr"
+                      } else if(movie.original_language == "vi"){
+                        movie.original_language = "vn";
+                      }else if(movie.original_language == "et"){
+                        movie.original_language = "ee";
+                      }else if(movie.original_language == "ja"){
+                        movie.original_language = "jp";
+                      }else if(movie.original_language == "da"){
+                        movie.original_language = "dk";
+                      } else if(movie.original_language == "hu"){
+                        movie.original_language = "ua";
+                      }
                 });
             })
         },
